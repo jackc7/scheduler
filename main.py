50f90b2c7bf9a61207a4e26d577141770d3d2a01
Jack Curran <60433661+jackc7@users.noreply.github.com>
2020-09-15 03:41:08 -0400

    Update main.py

--- main.py
+++ main.py
@@ -15,11 +15,13 @@ SCOPES = ["https://www.googleapis.com/auth/calendar"]
 calendar_id = "hmdrajoibklf4k59n4ua0ke8i4@group.calendar.google.com" # Main calendar is "primary"
 
 """Instructions to get started:
-run quickstart.py, make sure credentials.json is in the correct folder
-the .gitignore file prevents git from recognizing personal files
+Run quickstart.py, make sure credentials.json is in working directory.
+This creates token.pickle so you can authenticate with calendar api.
 
-The code I currently have creates a test event in my personal calendar
-Change calendar_id to your own
+The .gitignore file prevents git from pushing personal files.
+
+The code I currently have creates a test event in my personal calendar.
+Change calendar_id to your own.
 """
 
 with open("token.pickle", "rb") as token:
