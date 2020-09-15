4d159e0a3e1bd7aaec143deea60aa75a1727a2cc
Jack Curran <jack7curran@gmail.com>
2020-09-15 13:55:26 -0400

    Fixed

--- event_creator.py
+++ event_creator.py
@@ -5,3 +5,18 @@ from googleapiclient.discovery import build
 import datetime
 import os.path
 import pickle
+
+
+def new(event: dict, calendar_id="primary"):
+    SCOPES = ["https://www.googleapis.com/auth/calendar"]
+
+    try:
+        with open("token.pickle", "rb") as token:
+            creds = pickle.load(token)
+    except FileNotFoundError:
+        print("'credentials.json' not found. Download it from https://developers.google.com/calendar/quickstart/python and run quickstart.py.")
+        exit()
+
+    service = build("calendar", "v3", credentials=creds)
+    service.events().insert(calendarId=calendar_id, body=event).execute()
+    
--- main.py
+++ main.py
@@ -1,16 +1,4 @@
-import datetime
-import pickle
-import os.path
-import quickstart
-from googleapiclient.discovery import build
-from google_auth_oauthlib.flow import InstalledAppFlow
-from google.auth.transport.requests import Request
-
-# https://developers.google.com/calendar/create-events
-# https://developers.google.com/calendar/quickstart/python
-
-SCOPES = ["https://www.googleapis.com/auth/calendar"]
-calendar_id = "primary" # Main calendar is "primary"
+import event_creator
 
 """Instructions to get started:
 Run quickstart.py, make sure credentials.json is in working directory.
@@ -20,21 +8,16 @@ The .gitignore file prevents git from pushing personal files.
 
 The code I currently have creates a test event in my personal calendar.
 Change calendar_id to your own.
+
+https://developers.google.com/calendar/create-events
+https://developers.google.com/calendar/quickstart/python
 """
 
-try:
-    with open("token.pickle", "rb") as token:
-        creds = pickle.load(token)
-except:
-    quickstart.main()
-    
-service = build("calendar", "v3", credentials=creds)
-
-
+# Template Event
 event = {
-    "summary": "Test",
+    "summary": "Hello",
     "location": "Dedham, MA, 02026",
-    "description": "Test",
+    "description": "This is a description",
     "start": {
         "dateTime": "2020-09-28T09:00:00-07:00",
         "timeZone": "America/New_York",
@@ -42,70 +25,8 @@ event = {
     "end": {
         "dateTime": "2020-09-28T17:00:00-07:00",
         "timeZone": "America/New_York",
-    },
+    }
 }
 
-service.events().insert(calendarId=calendar_id, body=event).execute()
-
-class Data:
-    """Days school is on and days its off.
-    Starts August 30, 2020
-    https://www.dedham.k12.ma.us/cms/lib/MA02213180/Centricity/ModuleInstance/2803/20%2021%20Dedham%20School%20Calendar.pdf
-    """
-
-    DAY = [0, 0, 0, 0, 0, 0, 0,
-           0, 0, 0, 0, 0, 0, 0,
-           0, 0, 0, 1, 1, 1, 0,
-           0, 1, 1, 1, 1, 1, 0,
-           0, 1, 1, 1, 1, 1, 0,
-           0, 1, 1, 1, 1, 1, 0,
-           0, 0, 1, 1, 1, 1, 0,
-           0, 1, 1, 1, 1, 1, 0,
-           0, 1, 1, 1, 1, 1, 0,
-           0, 1, 1, 1, 1, 1, 0,
-           0, 1, 1, 0, 1, 1, 0,
-           0, 1, 1, 1, 1, 1, 0,
-           0, 1, 1, 1, 0, 0, 0,
-           0, 1, 1, 1, 1, 1, 0,
-           0, 1, 1, 1, 1, 1, 0,
-           0, 1, 1, 1, 1, 1, 0,
-           0, 1, 1, 0, 0, 0, 0,
-           0, 0, 0, 0, 0, 0, 0,
-           0, 1, 1, 0, 1, 1, 0,
-           0, 1, 1, 1, 1, 1, 0,
-           0, 0, 1, 1, 1, 1, 0,
-           0, 1, 1, 1, 1, 1, 0,
-           0, 1, 1, 1, 1, 1, 0,
-           0, 1, 1, 1, 1, 1, 0,
-           0, 0, 0, 0, 0, 0, 0,
-           0, 1, 1, 1, 1, 1, 0,
-           0, 1, 1, 0, 1, 1, 0,
-           0, 1, 1, 1, 1, 1, 0,
-           0, 1, 1, 1, 1, 1, 0,
-           0, 1, 1, 1, 1, 1, 0,
-           0, 1, 1, 1, 1, 0, 0,
-           0, 1, 1, 1, 1, 1, 0,
-           0, 1, 1, 1, 1, 1, 0,
-           0, 0, 0, 0, 0, 0, 0,
-           0, 1, 1, 1, 1, 1, 0,
-           0, 1, 1, 1, 1, 1, 0,
-           0, 1, 1, 1, 1, 1, 0,
-           0, 1, 1, 1, 1, 1, 0,
-           0, 1, 1, 1, 1, 1, 0,
-           0, 0, 1, 1, 1, 1, 0,
-           0, 1, 1, 1, 1, 1, 0,
-           0, 1, 1, 1, 1, 1, 0,
-           0, 1, 1, 1, 1, 1, 0]
-
-    
-    PATTERN = [[1, 7, 6, 5, 4],
-               [3, 2, 1, 7, 6],
-               [5, 4, 3, 2, 1],
-               [7, 6, 5, 4, 3],
-               [2, 1, 7, 6, 5],
-               [4, 3, 2, 1, 7],
-               [6, 5, 4, 3, 2]]
-
-
 if __name__ == "__main__":
-    pass
+    event_creator.new(event)
